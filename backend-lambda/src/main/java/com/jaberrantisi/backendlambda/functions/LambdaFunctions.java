69d65fcfe6184b81a50928bdea50c34858082367
rantisi <rantisijaber@gmail.com>
2025-11-11 11:11:44 -0500

    Added architecture image

--- backend-lambda/src/main/java/com/jaberrantisi/backendlambda/functions/LambdaFunctions.java
+++ backend-lambda/src/main/java/com/jaberrantisi/backendlambda/functions/LambdaFunctions.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Base64;
